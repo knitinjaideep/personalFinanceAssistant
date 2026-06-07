50ff69614cb5895e4a2acb28e0a38595992ddeff
knitinjaideep <knitinjaideep@gmail.com>
2026-06-07 09:56:11 -0400

    Added a nice blur effect

--- finsight_ai/frontend-next/app/banking/page.tsx
+++ finsight_ai/frontend-next/app/banking/page.tsx
@@ -6,10 +6,16 @@ export default function BankingPage() {
       className="flex flex-col"
       style={{ marginTop: "var(--nav-height)", height: "calc(100dvh - var(--nav-height))" }}
     >
-      <div className="flex-1 min-h-0 overflow-y-auto">
+      <div
+        className="flex-1 min-h-0 overflow-y-auto"
+        style={{
+          maskImage: "linear-gradient(to bottom, transparent 0px, black 32px)",
+          WebkitMaskImage: "linear-gradient(to bottom, transparent 0px, black 32px)",
+        }}
+      >
         <div
           className="mx-auto w-full max-w-[1320px] pb-12"
-          style={{ paddingLeft: "var(--page-x-padding)", paddingRight: "var(--page-x-padding)", paddingTop: "0" }}
+          style={{ paddingLeft: "var(--page-x-padding)", paddingRight: "var(--page-x-padding)", paddingTop: "1.5rem" }}
         >
           <BankingPageClient />
         </div>
